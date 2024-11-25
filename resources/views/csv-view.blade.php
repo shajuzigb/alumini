da554953d32c644ca48b98e65b3a5626f9f6f9b1
shaju <shaju.vly@gmail.com>
2024-11-25 19:33:28 +0530

    3rd textbox added

--- resources/views/csv-view.blade.php
+++ resources/views/csv-view.blade.php
@@ -14,9 +14,8 @@
                     <td>{{ $row['id'] }}</td>
                     <td><input type="text" name="name" id="name" value="{{ $row['Dc,'] }}"></td>
                     <td><input type="text" name="amt1" id="amt1" value="{{ $row['amount1']}}"></td>
-'] }}">{{ $row['amount1'] }}</td>
-                    <td>{{ $row['Amount 2'] }}</td>
-                    <td>{{ $row['amount3'] }}</td>
+                    <td><input type="text" name="amt1" id="amt1" value="{{ $row['Amount 2']}}"></td>
+                     <td>{{ $row['amount3'] }}</td>
                
             </tr>
         @endforeach
