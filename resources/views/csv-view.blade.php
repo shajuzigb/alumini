d4e929b0f246c4cfda578dbd92344ccdbd052749
shaju <shaju.vly@gmail.com>
2024-11-28 21:37:16 +0530

    new changes

--- resources/views/csv-view.blade.php
+++ resources/views/csv-view.blade.php
@@ -14,7 +14,7 @@
                     <td>{{ $row['id'] }}</td>
                     <td><input type="text" name="name" id="name" value="{{ $row['Dc,'] }}"></td>
                     <td><input type="text" name="amt1" id="amt1" value="{{ $row['amount1']}}"></td>
-                    <td><input type="text" name="amt1" id="amt1" value="{{ $row['Amount 2']}}"></td>
+                    <td><input type="text" name="amt4" id="amt4" value="{{ $row['Amount 2']}}"></td>
                      <td>{{ $row['amount3'] }}</td>
                
             </tr>
